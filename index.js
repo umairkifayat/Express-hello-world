4f2f4066012e7a7fa557cb2d2953e235172932d6
umair kifayat <umairkifayat3@gmail.com>
2024-02-21 14:59:35 -0800

    updated

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,7 +5,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/login', (req, res) => {
+  res.send('hello dunya')
+})
 
-app.listen(port, () => {
+app.listen(port , () => {
   console.log(`Example app listening on port ${port}`)
 })
